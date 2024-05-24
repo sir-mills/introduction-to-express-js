0ec6c7c9c9a19e746189585ed6ee30ec01d3fb26
Etiosa <115146462+sir-mills@users.noreply.github.com>
2024-05-24 04:57:03 -0600

    server set

--- src/app.js
+++ src/app.js
@@ -4,3 +4,16 @@ import bodyParser from "body-parser";
 
 const app = express();
 const port = 3030;
+
+app.use(morgan("dev"));
+app.use(bodyParser.json());
+
+app.get("/", (req, res) => {
+  req.status(200).send({
+    message: "hello-world from express",
+  });
+});
+
+app.listen(port, () => {
+  console.log(`server is running on ${port}`);
+});
